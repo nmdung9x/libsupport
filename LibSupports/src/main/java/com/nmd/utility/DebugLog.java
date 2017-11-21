da381154d9cd711ca6b04c4c9206ebfcdd161524
NMD9x <manhdung.nguyen@vnnplus.vn>
2017-11-21 15:19:14 +0700

    new NetworkService

---
+++ LibSupports/src/main/java/com/nmd/utility/DebugLog.java
@@ -0,0 +1,321 @@
+package com.nmd.utility;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.nmd.utility.other.ZNetworkData;
+import com.nmd.utility.other.ZUploadLogService;
+import com.nmd.utility.other.ZUploadLogService.OnUploadLogResult;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.Log;
+
+public class DebugLog {
+	enum Type {
+		v,
+		d,
+		i,
+		n,
+		w,
+		e
+	}
+	
+	public static void log(Type type, String tag, String msg, Object obj, boolean upOnline){
+    	String message = "";
+    	boolean isException = false;
+    	if (obj != null) {
+    		if (obj instanceof Exception) {
+        		isException = true;
+        		Exception e = (Exception) obj;
+        		StringWriter errors = new StringWriter();
+            	e.printStackTrace(new PrintWriter(errors));
+            	
+            	message = msg.trim().isEmpty() ? errors.toString() : msg.trim()+"\n"+errors.toString();
+        	} else if (obj instanceof Throwable) {
+        		isException = true;
+        		Throwable e = (Throwable) obj;
+        		StringWriter errors = new StringWriter();
+            	e.printStackTrace(new PrintWriter(errors));
+            	
+            	message = msg.trim().isEmpty() ? errors.toString() : msg.trim()+"\n"+errors.toString();
+        	} else {
+        		isException = false;
+        		message = msg.trim().isEmpty() ? String.valueOf(obj).trim() : msg.trim()+"\n"+String.valueOf(obj).trim();
+        	}
+    	} else {
+    		isException = false;
+    		message = msg.trim();
+    	}
+ 
+    	
+    	StackTraceElement stack = Thread.currentThread().getStackTrace()[4];
+        String fullClassName = stack.getClassName();
+        String className = fullClassName.substring(fullClassName.lastIndexOf(".") + 1);
+
+        if (className.contains("$")) {
+            className = className.substring(0, className.lastIndexOf("$"));
+        }
+
+        String methodName = stack.getMethodName();
+        int lineNumber = stack.getLineNumber();
+        
+        String check = "at ("+ className + ".java:" + lineNumber + ") " + "[" + methodName + "] ";
+        
+        if (isException) {
+        	switch (type) {
+			case v:
+				if (UtilityMain.SHOW_LOG_D) {
+					Log.v(tag, check + message);
+				}
+				if (UtilityMain.DEBUG_D) {
+		            appendLog("Log---v " + check + message, upOnline, tag);
+		        }
+				break;
+
+			case d:
+				if (UtilityMain.SHOW_LOG_D) {
+					Log.d(tag, check + message);
+				}
+				if (UtilityMain.DEBUG_D) {
+		            appendLog("Log---d " + check + message, upOnline, tag);
+		        }
+				break;
+
+			case i:
+				if (UtilityMain.SHOW_LOG_I) {
+					Log.i(tag, check + message);
+				}
+				if (UtilityMain.DEBUG_I) {
+		            appendLog("Log---i " + check + message, upOnline, tag);
+		        }
+				break;
+
+			case n:
+				if (UtilityMain.SHOW_LOG_N) {
+					Log.i(tag, check + message);
+				}
+				if (UtilityMain.DEBUG_N) {
+		            appendLog("Log---n " + check + message, upOnline, tag);
+		        }
+				break;
+
+			case w:
+				if (UtilityMain.SHOW_LOG_I) {
+					Log.w(tag, check + message);
+				}
+				if (UtilityMain.DEBUG_I) {
+		            appendLog("Log---w " + check + message, upOnline, tag);
+		        }
+				break;
+
+			case e:
+				if (UtilityMain.SHOW_LOG_E) {
+					Log.e(tag, check + message);
+				}
+				if (UtilityMain.DEBUG_E) {
+		            appendLog("Log---e " + check + message, upOnline, tag);
+		        }
+				break;
+
+			default:
+				break;
+			}
+        } else {
+        	switch (type) {
+			case v:
+				if (UtilityMain.SHOW_LOG_D && UtilityMain.SHOW_LOG_E_EXCEPTION) {
+					Log.v(tag, check + message);
+				}
+				if (UtilityMain.DEBUG_D && UtilityMain.DEBUG_E_EXCEPTION) {
+		            appendLog("Log---v " + check + message, upOnline, tag);
+		        }
+				break;
+
+			case d:
+				if (UtilityMain.SHOW_LOG_D && UtilityMain.SHOW_LOG_E_EXCEPTION) {
+					Log.d(tag, check + message);
+				}
+				if (UtilityMain.DEBUG_D && UtilityMain.DEBUG_E_EXCEPTION) {
+		            appendLog("Log---d " + check + message, upOnline, tag);
+		        }
+				break;
+
+			case i:
+				if (UtilityMain.SHOW_LOG_I && UtilityMain.SHOW_LOG_E_EXCEPTION) {
+					Log.i(tag, check + message);
+				}
+				if (UtilityMain.DEBUG_I && UtilityMain.DEBUG_E_EXCEPTION) {
+		            appendLog("Log---i " + check + message, upOnline, tag);
+		        }
+				break;
+
+			case n:
+				if (UtilityMain.SHOW_LOG_N && UtilityMain.SHOW_LOG_E_EXCEPTION) {
+					Log.i(tag, check + message);
+				}
+				if (UtilityMain.DEBUG_N && UtilityMain.DEBUG_E_EXCEPTION) {
+		            appendLog("Log---n " + check + message, upOnline, tag);
+		        }
+				break;
+
+			case w:
+				if (UtilityMain.SHOW_LOG_I && UtilityMain.SHOW_LOG_E_EXCEPTION) {
+					Log.w(tag, check + message);
+				}
+				if (UtilityMain.DEBUG_I && UtilityMain.DEBUG_E_EXCEPTION) {
+		            appendLog("Log---w " + check + message, upOnline, tag);
+		        }
+				break;
+
+			case e:
+				if (UtilityMain.SHOW_LOG_E_EXCEPTION) {
+					Log.e(tag, check + message);
+				}
+				if (UtilityMain.DEBUG_E_EXCEPTION) {
+		            appendLog("Log---e " + check + message, upOnline, tag);
+		        }
+				break;
+
+			default:
+				break;
+			}
+        }
+        
+    }
+	
+    public static void logv(Object obj){
+        log(Type.v, UtilityMain.TAG, "", obj, false);
+    }
+    
+    public static void logv(String tag, Object obj){
+    	log(Type.v, tag, "", obj, false);
+    }
+    
+    public static void logv(String tag, String msg, Object obj){
+    	log(Type.v, tag, msg, obj, false);
+    }
+	
+    public static void logd(Object obj){
+        log(Type.d, UtilityMain.TAG, "", obj, false);
+    }
+    
+    public static void logd(String tag, Object obj){
+    	log(Type.d, tag, "", obj, false);
+    }
+    
+    public static void logd(String tag, String msg, Object obj){
+    	log(Type.d, tag, msg, obj, false);
+    }
+	
+    public static void logi(Object obj){
+        log(Type.i, UtilityMain.TAG, "", obj, false);
+    }
+    
+    public static void logi(String tag, Object obj){
+    	log(Type.i, tag, "", obj, false);
+    }
+    
+    public static void logi(String tag, String msg, Object obj){
+    	log(Type.i, tag, msg, obj, false);
+    }
+	
+    public static void logn(Object obj){
+        log(Type.n, UtilityMain.TAG, "", obj, false);
+    }
+    
+    public static void logn(String tag, Object obj){
+    	log(Type.n, tag, "", obj, false);
+    }
+    
+    public static void logn(String tag, String msg, Object obj){
+    	log(Type.n, tag, msg, obj, false);
+    }
+	
+    public static void logw(Object obj){
+        log(Type.w, UtilityMain.TAG, "", obj, false);
+    }
+    
+    public static void logw(String tag, Object obj){
+    	log(Type.w, tag, "", obj, false);
+    }
+    
+    public static void logw(String tag, String msg, Object obj){
+    	log(Type.w, tag, msg, obj, false);
+    }
+	
+    public static void loge(Object obj){
+        log(Type.e, UtilityMain.TAG, "", obj, false);
+    }
+    
+    public static void loge(String tag, Object obj){
+    	log(Type.e, tag, "", obj, false);
+    }
+    
+    public static void loge(String tag, String msg, Object obj){
+    	log(Type.e, tag, msg, obj, false);
+    }
+    
+    public static void log_online(Object obj){
+    	log(Type.i, "", "", obj, true);
+    }
+    
+    public static void log_online(String filename, Object obj){
+    	log(Type.i, filename, "", obj, true);
+    }
+    
+	static void appendLog(String text, boolean upOnline, String filename) {
+		if (upOnline && UtilityMain.DEBUG_ONLINE && !text.trim().isEmpty()) {
+			if (filename.trim().isEmpty()) filename = UtilityMain.zfilename();
+			Context context = UtilityMain.mContext;
+			if (context == null) {
+				DebugLog.loge("Context null!");
+			} else {
+				new ZUploadLogService().uploadLogV2(context, ZNetworkData.API_UPLOG_ONLINE,  ZNetworkData.uploadlog(filename, text), new OnUploadLogResult() {
+
+					@Override
+					public void uploadLogMethod(boolean isSuccess) {}
+				});
+			}
+
+		}
+		if (!UtilityMain.isRecordLog) {
+			return;
+		}
+		
+		boolean isNew = false;
+		
+		File logFile = new File(UtilityMain.path + "/" + UtilityMain.TAG + ".log");
+		if (!logFile.exists()) {
+			try {
+				logFile.createNewFile();
+				isNew = true;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true));
+			if (isNew) {
+				buf.append(UtilLibs.getInfoDevices()).append('\n');
+			}
+			buf.append(getCurrentTime() + " (" + UtilLibs.getTimeZoneInLocal() + ")" + "_" + text).append('\n');
+			buf.newLine();
+			buf.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+    
+    @SuppressLint("SimpleDateFormat")
+	private static String getCurrentTime(){
+    	SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm:ss");
+        Date resultdate = new Date(System.currentTimeMillis());
+		return sdf.format(resultdate);
+    }
+}
---
+++ LibSupports/src/main/java/com/nmd/utility/NetworkService.java
@@ -0,0 +1,309 @@
+package com.nmd.utility;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.nmd.utility.other.MultipartRequest;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by nmd9x on 10/12/17.
+ */
+
+public class NetworkService extends ContextWrapper {
+    Context context = null;
+    private RequestQueue mRequestQueue = null;
+
+    public NetworkService(Context ctx) {
+        super(ctx);
+        context = ctx;
+
+        mRequestQueue = Volley.newRequestQueue(context);
+    }
+
+    public interface OnGetResult {
+        void result(String response);
+    }
+
+    public void get(final String url, final HashMap<String, String> headers, final OnGetResult callback) {
+        get(url, headers, "", false, callback);
+    }
+
+    public void get(final String url, final HashMap<String, String> headers, String tagRequest, boolean singleRequest, final OnGetResult callback) {
+        DebugLog.logn("url:\n"+url);
+        if (url.isEmpty()) {
+            DebugLog.loge("url empty");
+            return;
+        }
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                DebugLog.logn(response);
+                callback.result(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                DebugLog.loge(error);
+                callback.result(error.getMessage());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                if (headers == null) {
+                    return new HashMap<>();
+                }
+                return headers;
+            }
+        };
+
+        stringRequest.setShouldCache(false);
+        if (singleRequest) {
+            if (!tagRequest.trim().isEmpty()) {
+                mRequestQueue.cancelAll(tagRequest);
+            }
+        }
+        if (!tagRequest.trim().isEmpty()) stringRequest.setTag(tagRequest);
+        mRequestQueue.add(stringRequest);
+    }
+
+    public void post(String url, final String body, final HashMap<String, String> headers, final OnGetResult callback) {
+        post(url, body, headers, "", false, callback);
+    }
+
+    public void post(String url, final String body, final HashMap<String, String> headers, String tagRequest, boolean singleRequest, final OnGetResult callback) {
+        if (url.isEmpty()) {
+            DebugLog.loge("url empty");
+            return;
+        }
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                DebugLog.logn(response);
+                callback.result(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                DebugLog.loge(error);
+                callback.result(error.getMessage());
+            }
+        }) {
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    if (body.isEmpty()) return null;
+                    return body.getBytes("utf-8");
+                } catch (Exception e) {
+                    DebugLog.loge(e);
+                    return null;
+                }
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                if (headers == null) {
+                    return new HashMap<>();
+                }
+                return headers;
+            }
+        };
+
+        stringRequest.setShouldCache(false);
+        if (singleRequest) {
+            if (!tagRequest.trim().isEmpty()) mRequestQueue.cancelAll(tagRequest);
+        }
+        if (!tagRequest.trim().isEmpty()) stringRequest.setTag(tagRequest);
+        mRequestQueue.add(stringRequest);
+    }
+
+    public void post(String url, final HashMap<String, String> params, final HashMap<String, String> headers, final OnGetResult callback) {
+        post(url, params, headers, "", false, callback);
+    }
+
+    public void post(String url, final HashMap<String, String> params, final HashMap<String, String> headers, String tagRequest, boolean singleRequest, final OnGetResult callback) {
+        if (url.isEmpty()) {
+            DebugLog.loge("url empty");
+            return;
+        }
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                DebugLog.logn(response);
+                callback.result(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                DebugLog.loge(error);
+                callback.result(error.getMessage());
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                if (params == null) {
+                    return new HashMap<>();
+                }
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                if (headers == null) {
+                    return new HashMap<>();
+                }
+                return headers;
+            }
+        };
+
+        stringRequest.setShouldCache(false);
+        if (singleRequest) {
+            if (!tagRequest.trim().isEmpty()) mRequestQueue.cancelAll(tagRequest);
+        }
+        if (!tagRequest.trim().isEmpty()) stringRequest.setTag(tagRequest);
+        mRequestQueue.add(stringRequest);
+    }
+
+    public void post(String url, final HashMap<String, String> params, final HashMap<String, File> files, final HashMap<String, String> headers, final OnGetResult callback) {
+        post(url, params, files, headers, "", false, callback);
+    }
+
+    public void post(String url, final HashMap<String, String> params, final HashMap<String, File> files, final HashMap<String, String> headers, String tagRequest, boolean singleRequest, final OnGetResult callback) {
+        if (url.isEmpty()) {
+            DebugLog.loge("url empty");
+            return;
+        }
+        byte[] multipartBody = multipartBody(params, files);
+
+        if (multipartBody == null) {
+            DebugLog.loge("multipartBody == null");
+            callback.result("multipartBody == null");
+            return;
+        }
+
+        MultipartRequest multipartRequest = new MultipartRequest(url, null, mimeType, multipartBody, new Response.Listener<NetworkResponse>() {
+            @Override
+            public void onResponse(NetworkResponse response) {
+                try {
+                    String result = new String(response.data, "UTF-8");
+                    callback.result(result);
+                } catch (Exception e) {
+                    DebugLog.loge(e);
+                    callback.result(e.getMessage());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                DebugLog.loge(error);
+                callback.result(error.getMessage());
+            }
+        });
+
+        multipartRequest.setShouldCache(false);
+        if (singleRequest) {
+            if (!tagRequest.trim().isEmpty()) mRequestQueue.cancelAll(tagRequest);
+        }
+        if (!tagRequest.trim().isEmpty()) multipartRequest.setTag(tagRequest);
+        mRequestQueue.add(multipartRequest);
+    }
+
+    public byte[] multipartBody(HashMap<String, String> params, HashMap<String, File> files){
+        byte[] multipartBody = null;
+
+        boundary = "apiclient-" + System.currentTimeMillis();
+        mimeType = "multipart/form-data;boundary=" + boundary;
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(bos);
+
+        try {
+            if (params.size() > 0) {
+                for (HashMap.Entry<String,String> entry : params.entrySet()) {
+                    buildTextPart(dos, entry.getKey(), entry.getValue());
+                }
+            }
+            if (files.size() > 0) {
+                for (HashMap.Entry<String,File> entry : files.entrySet()) {
+                    String path = entry.getValue().getPath();
+                    DebugLog.loge("path:\n" +path);
+                    buildPart(dos, entry.getKey(), UtilLibs.getFileNameAndExtension(path), readFile(entry.getValue()));
+                }
+            }
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+            multipartBody = bos.toByteArray();
+        } catch (Exception e) {
+            DebugLog.loge(e);
+        }
+
+        return multipartBody;
+    }
+
+    static byte[] readFile(File file) {
+        int size = (int) file.length();
+        byte[] bytes = new byte[size];
+
+        try {
+            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
+            buf.read(bytes, 0, bytes.length);
+            buf.close();
+        } catch (Exception e) {
+            DebugLog.loge(e);
+        }
+        return bytes;
+    }
+
+    final String twoHyphens = "--";
+    final String lineEnd = "\r\n";
+    String boundary = "apiclient-" + System.currentTimeMillis();
+    String mimeType = "multipart/form-data;boundary=" + boundary;
+
+    void buildTextPart(DataOutputStream dataOutputStream, String parameterName, String parameterValue) throws Exception {
+        dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
+        dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"" + parameterName + "\"" + lineEnd);
+        dataOutputStream.writeBytes("Content-Type: text/plain; charset=UTF-8" + lineEnd);
+        dataOutputStream.writeBytes(lineEnd);
+        dataOutputStream.write(String.valueOf(parameterValue + lineEnd).getBytes("UTF-8"));
+    }
+
+    void buildPart(DataOutputStream dataOutputStream, String parameterName, String fileName, byte[] fileData) throws Exception {
+        dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
+        dataOutputStream.writeBytes("Content-Disposition: form-data; name=\""+parameterName+"\"; filename=\"" + fileName + "\"" + lineEnd);
+        dataOutputStream.writeBytes(lineEnd);
+
+        ByteArrayInputStream fileInputStream = new ByteArrayInputStream(fileData);
+        int bytesAvailable = fileInputStream.available();
+
+        int maxBufferSize = 1024 * 1024;
+        int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+        byte[] buffer = new byte[bufferSize];
+
+        // read file and write it into form...
+        int bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+        while (bytesRead > 0) {
+            dataOutputStream.write(buffer, 0, bufferSize);
+            bytesAvailable = fileInputStream.available();
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+        }
+
+        dataOutputStream.writeBytes(lineEnd);
+    }
+}
