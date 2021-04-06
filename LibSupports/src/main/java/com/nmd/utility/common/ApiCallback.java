2de4d6b13df0ea27cf537a5d67c8b2b33b25a45f
Nguyễn Mạnh Dũng <dungnm@sapo.vn>
2021-04-06 10:56:30 +0700

    update v3.2.2: optimize Network Callback

--- LibSupports/src/main/java/com/nmd/utility/common/ApiCallback.java
+++ LibSupports/src/main/java/com/nmd/utility/common/ApiCallback.java
@@ -11,32 +11,19 @@ import retrofit2.Call;
 import retrofit2.Callback;
 
 public abstract class ApiCallback<T> implements Callback<T> {
-    abstract public void onResponse(Call<T> call, boolean isSuccess, int statusCode, String responseText, @Nullable String requestText, String requestUrl, ErrorR error, boolean isCancelled);
+    abstract public void onResponse(Call<T> call, boolean isSuccess, int statusCode, T response, @Nullable String requestText, String requestUrl, ErrorR error, boolean isCancelled);
 
     @Override
-    public void onResponse(@NonNull Call<T> call, @NonNull retrofit2.Response<T> response) {
-        log(call, response.code());
-        String responseText = "";
-        if (response.body() != null) {
-            responseText = response.body().toString();
-        } else {
-            if (response.errorBody() != null) {
-                try {
-                    responseText = response.errorBody().string();
-                } catch (Exception e) {
-                    DebugLog.loge(e);
-                    responseText = "";
-                }
-            }
-        }
-        onResponse(call, response.isSuccessful(), response.code(), responseText, bodyToString(call.request().body()), call.request().url().toString(), new ErrorR(response.errorBody(), response.code(), null), call.isCanceled());
+    public void onResponse(@NonNull Call<T> call, @NonNull retrofit2.Response<T> responseTmp) {
+        log(call, responseTmp.code());
+        onResponse(call, responseTmp.isSuccessful(), responseTmp.code(), responseTmp.body(), bodyToString(call.request().body()), call.request().url().toString(), new ErrorR(responseTmp.errorBody(), responseTmp.code(), null), call.isCanceled());
     }
 
     @Override
     public void onFailure(@NonNull Call<T> call, @NonNull Throwable t) {
         log(call, -1);
 
-        onResponse(call, false, -1, "", bodyToString(call.request().body()), call.request().url().toString(), new ErrorR("", -1, t), call.isCanceled());
+        onResponse(call, false, -1, null, bodyToString(call.request().body()), call.request().url().toString(), new ErrorR("", -1, t), call.isCanceled());
     }
 
     private void log(Call<T> call, int statusCode) {
