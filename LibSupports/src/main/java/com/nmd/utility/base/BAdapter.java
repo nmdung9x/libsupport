98f68720003c508af06238835a28a41dda2611ae
dungnm <>
2022-11-04 10:55:51 +0700

    v3.4.1 update base adapter

--- LibSupports/src/main/java/com/nmd/utility/base/BAdapter.java
+++ LibSupports/src/main/java/com/nmd/utility/base/BAdapter.java
@@ -1,6 +1,7 @@
 package com.nmd.utility.base;
 
 import android.content.Context;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,14 +13,35 @@ import com.nmd.utility.viewbinder.LayoutBinder;
 
 import java.util.ArrayList;
 
-
 public abstract class BAdapter<T> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     Context c;
     ArrayList<T> l;
+    boolean loadMore;
+    boolean loadMoreEnable;
+    OnLoadMoreListener lm;
+    public interface OnLoadMoreListener {
+        public void onLoadMore();
+    }
 
     public BAdapter(Context context, ArrayList<T> list) {
         c = context;
         l = list;
+        lm = null;
+    }
+
+    public BAdapter(Context context, ArrayList<T> list, OnLoadMoreListener loadMoreListener) {
+        c = context;
+        l = list;
+        lm = loadMoreListener;
+        loadMoreEnable = true;
+    }
+
+    public void setLoadMoreEnable(boolean enable) {
+        loadMoreEnable = enable;
+    }
+
+    public void loadMoreComplete() {
+        loadMore = false;
     }
 
     @Override
@@ -38,6 +60,12 @@ public abstract class BAdapter<T> extends RecyclerView.Adapter<RecyclerView.View
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder mViewHolder, final int position) {
         initViewItem(c, mViewHolder, l.get(position), position);
+        if (position == l.size()-1 && lm != null && loadMoreEnable && !loadMore) {
+            loadMore = true;
+            new Handler().postDelayed(() -> {
+                lm.onLoadMore();
+            }, 500);
+        }
     }
 
     public abstract RecyclerView.ViewHolder getViewHolder(View viewInflater);
